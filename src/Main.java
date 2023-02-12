a1a7e0fc1d0534c846506579df355feff46e6a86
arxip <arxipov1989@gmail.com>
2023-02-12 16:17:18 +0300

    курсовая тест

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,26 @@
+import java.util.Random;
 
 public class Main {
     private static Employee[] employees = new Employee[10];
+
     public static void main(String[] args) {
-
+        printEmployeeInfo();
 
 
     }
-    private static Employee doEmployees(){
+
+    private static Random doEmployees() {
+        Random random = new Random();
         for (int i = 0; i < employees.length; i++) {
-            employees[i] = new Employee()}
+
+            employees[i] = new Employee("Сотрудник" + (i + 1), random.nextInt(1, 6), random.nextInt(50000, 100000));
+        }return random;
 
 
 
     }
+
+
     private static void printEmployeeInfo(){
         for (Employee employee: employees) {
             System.out.println(employee);}
